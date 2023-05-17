4c9834ecfede13e9e3ab692f7adcd248f0a92767
Judit Vives <jvivesramon@gmail.com>
2023-05-17 22:12:22 +0200

    Create express aplication and call the listen method for localhost

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import "./loadEnviroment.js";
+import createDebug from "debug";
+import chalk from "chalk";
+import app from "./server/index.js";
+
+const debug = createDebug("users-api:root");
+
+const port = process.env.PORT ?? 4000;
+const localhost = `http://localhost:${port}`;
+
+app.listen(port, () => {
+  debug(`Listening on ${chalk.bgBlueBright(localhost)}`);
+});
