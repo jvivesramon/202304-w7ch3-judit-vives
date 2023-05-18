bd43fa1583736684127056c3754b3046f0a0d304
Judit Vives <jvivesramon@gmail.com>
2023-05-18 21:05:27 +0200

    Add generalError controller

--- src/server/middlewares/errorControllers/errorControllers.ts
+++ src/server/middlewares/errorControllers/errorControllers.ts
@@ -2,6 +2,7 @@ import { type NextFunction, type Request, type Response } from "express";
 import "../../../loadEnviroment.js";
 import createDebug from "debug";
 import CustomError from "../../../CustomError/CustomError.js";
+import chalk from "chalk";
 
 const debug = createDebug("users-api:server:middleware:errorControllers");
 
@@ -15,4 +16,18 @@ export const notFoundError = (
   debug(error.message);
 
   next(error);
+};
+
+export const generalError = (
+  error: CustomError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  debug(chalk.red(error.message));
+
+  const statusCode = error.statusCode || 500;
+  const message = error.statusCode ? error.message : "Everything has explode";
+
+  res.status(statusCode).json({ message });
 };
