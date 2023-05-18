77fb79480ddbfa3a7c9526576c113f2df4f75380
Judit Vives <jvivesramon@gmail.com>
2023-05-18 19:39:53 +0200

    Add mongoose connection and guard for exit

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,28 @@ import "./loadEnviroment.js";
 import createDebug from "debug";
 import chalk from "chalk";
 import app from "./server/index.js";
+import connectToDatabase from "./database/connectDatabase.js";
 
 const debug = createDebug("users-api:root");
 
 const port = process.env.PORT ?? 4000;
+const mongoDbConnection = process.env.MONGODB_CONNECTION;
+
 const localhost = `http://localhost:${port}`;
+
+if (!mongoDbConnection) {
+  debug(chalk.red(`Missign enviroment variables`));
+  process.exit(1);
+}
 
 app.listen(port, () => {
   debug(`Listening on ${chalk.bgBlueBright(localhost)}`);
 });
+
+try {
+  await connectToDatabase(mongoDbConnection);
+
+  debug(chalk.blueBright("Connected to database"));
+} catch (error: unknown) {
+  debug(`Error connecting to database: ${chalk.red((error as Error).message)}`);
+}
