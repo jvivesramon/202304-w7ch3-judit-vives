5ad497f850493e57ddd514f203b5d3860db0b022
Judit Vives <jvivesramon@gmail.com>
2023-05-19 07:51:12 +0200

    Add types for user credentials

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import {
+  generalError,
+  notFoundError,
+} from "./middlewares/errorControllers/errorControllers.js";
+import { userRouter } from "./routers/user/userRouter.js";
 
 const app = express();
 
@@ -8,5 +13,11 @@ app.disable("x-powered-by");
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use("/user", userRouter);
+
+app.use(notFoundError);
+
+app.use(generalError);
 
 export default app;
